9d1c370ce04bf6ecf5f2322de6770feb661593ba
Emiliano Rios <emirioslp@gmail.com>
2025-04-15 00:44:22 -0300

    [Parte 3.4] Utilizamos un sistema de plantillas, tuve un error puse el archivo como detailS plural y estuve como 30 min para encontrar el error

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,11 +1,6 @@
-from django.http import HttpResponse
-from django.shortcuts import get_object_or_404, render
-
+from django.shortcuts import render, get_object_or_404
 from .models import Question
-
-from django.shortcuts import render
-from django.http import Http404
-
+from django.http import HttpResponse
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
@@ -13,17 +8,12 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question": question})
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s." % question_id)
-
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "polls/detail.html", {"question": question})
